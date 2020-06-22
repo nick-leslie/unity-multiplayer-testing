444b0b1425cb1f4d70e3457080bf74c7956d4380
swordcom367 <46449945+swordcom367@users.noreply.github.com>
2020-06-22 17:14:36 -0400

    added in genaration

---
+++ gun gollem/Assets/CreateText.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public static TextMesh createWorldText(Transform parent, string text,Vector3 localpos,int fountSize,Color color,TextAnchor textAnchor, TextAlignment textAlignment,int sortingOrder)
+    {
+        GameObject gameObject = new GameObject("world_text", typeof(TextMesh));
+        Transform transform = gameObject.transform;
+        transform.SetParent(parent, false);
+        transform.localPosition = localpos;
+        TextMesh textMesh = gameObject.GetComponent<TextMesh>();
+        textMesh.text = text;
+        textMesh.anchor = textAnchor;
+        textMesh.alignment = textAlignment;
+        textMesh.fontSize = fountSize;
+        textMesh.fontSize = fountSize;
+        textMesh.color = color;
+        textMesh.GetComponent<MeshRenderer>().sortingOrder = sortingOrder;
+        return textMesh;
+
+    }
+}
---
+++ gun gollem/Assets/dungionGenaration.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dungionGenaration : MonoBehaviour
+{
+    public int runs=10;
+    public GameObject room;
+    public List<Vector2Int> visted = new List<Vector2Int>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        genarate();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            genarate();
+        }
+    }
+    void genarate()
+    {
+        Grid grid = new Grid(10, 10, 10f);
+        Vector2Int pos = new Vector2Int(5, 5);
+        grid.setNum(pos, 1);
+        Instantiate(room, grid.getCenterOfCell(pos), room.transform.rotation);
+        visted.Add(pos);
+        for (int i = 0; i < runs; i++)
+        {
+            bool seen = false;
+            pos += RandomDire();
+            for (int j = 0; j < visted.Count; j++)
+            {
+                if (Vector2Int.Distance(pos, visted[j]) == 0)
+                {
+                    Debug.Log("got");
+                    seen = true;
+                }
+            }
+            //Debug.Log(pos);
+            if (seen == false)
+            {
+                grid.setNum(pos, grid.GetNum(pos) + 1);
+                visted.Add(pos);
+                Instantiate(room, grid.getCenterOfCell(pos), room.transform.rotation);
+            }
+            else
+            {
+                i -= 1;
+            }
+        }
+        //Grid tiles = new Grid(40, 20, 1f);
+    }
+    Vector2Int RandomDire()
+    {
+        int x=0;
+        int y=0;
+        int coinFlip;
+        //see wich random is chosen first
+        coinFlip = Random.Range(0, 2);
+        Debug.Log(coinFlip);
+        if (coinFlip < 1)
+        {
+            //X has priority  
+            x = Random.Range(-1, 2);
+            while (x == 0 && y == 0)
+            {
+                x = Random.Range(-1, 2);
+            }
+        }
+        else
+        {
+            y = Random.Range(-1, 2);
+
+            while (x == 0 && y == 0)
+            {
+                y = Random.Range(-1, 2);
+            }
+        }
+        return new Vector2Int(x, y);
+    }
+}
